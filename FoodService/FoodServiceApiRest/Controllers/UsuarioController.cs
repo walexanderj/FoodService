d1558ef5481606fa38daac275f7b3257c39b887f
ALEXANDER JARAMILLO <walexanderj@hotmail.es>
2020-05-23 07:46:54 -0500

    Se creó proyecto FoodServiceApiRest

---
+++ FoodService/FoodServiceApiRest/Controllers/UsuarioController.cs
@@ -0,0 +1,44 @@
+﻿using FoodServiceApiRest.Models;
+using FoodServiceApiRest.app_code;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FoodServiceApiRest.Data;
+
+namespace FoodServiceApiRest.Controllers
+{
+    public class UsuarioController : ApiController
+    {
+        // GET: api/Usuario
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Usuario/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Usuario
+        public void Post([FromBody]string value)
+        {
+            UsuarioPersistence up = new UsuarioPersistence();
+            UsuarioModel usuario = new UsuarioModel();
+        }
+
+        // PUT: api/Usuario/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Usuario/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ FoodService/FoodServiceApiRest/Models/UsuarioModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FoodServiceApiRest.Models
+{
+    public class UsuarioModel
+    {
+        long Id { get; set; }
+        String UserName { get; set; }
+        String UserFullName { get; set; }
+        string Password { get; set;}
+    }
+}
