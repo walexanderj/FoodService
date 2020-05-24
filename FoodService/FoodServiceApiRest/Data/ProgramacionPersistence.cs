6c2198ee8ce50f2950a1af32924440c7ebd46aca
ALEXANDER JARAMILLO <walexanderj@hotmail.es>
2020-05-24 13:12:36 -0500

    Actualizacion del programa

--- FoodService/FoodServiceApiRest/Data/ProgramacionPersistence.cs
+++ FoodService/FoodServiceApiRest/Data/ProgramacionPersistence.cs
@@ -42,6 +42,7 @@ namespace FoodServiceApiRest.Data
                     obj.Turno = (string)reader["Turno"];
                     obj.IdPeriodo = (int)reader["idPeriodo"];
                     list.Add(obj);
+
                 }
                 return list;
             }
